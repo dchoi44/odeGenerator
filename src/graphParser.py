39c883b6a7863d1fe673c86c767a003f7df57fd6
Dongho Choi <zmal0103@gmail.com>
2017-08-14 04:31:53 +0200

    Generalizing the code

--- src/graphParser.py
+++ src/graphParser.py
@@ -1,6 +1,5 @@
 import random
 
-# Voter class is legacy now, not used in the main function.
 class Voter:
 	def __init__(self, opinion, uid):
 		self.__opinion = opinion
@@ -26,32 +25,87 @@ class Voter:
 	def visiting(self):
 		self.__visited = True
 
-class UserDict:
+class Relationship:
+	def __init__(self, id_from, id_to):
+		self.__nodes = id_from, id_to
+
+	def get_nodes(self):
+		return self.__nodes
+
+class Usr_dict:
 	def __init__(self):
 		self.dict = {}
 
-	def push_usr(self, uid):
+	def push_usr(self, uid, cond_list):
 		if uid in self.dict.keys():
 			return self.dict[uid]
 		else:
-			self.dict[uid] = Voter(int((random.random() * 3 + 1) / 1) - 2, uid)
+			self.dict[uid] = Voter(cond_list.pop(), uid)
 			return self.dict[uid]
 
+class Edge_dict:
+	def __init__(self):
+		self.__dict = {}
+		self.__length = 0
 
-def parser_main(fname, directed = False):
+	def push_edge(self, id_from, id_to):
+		self.__dict[self.__length] = Relationship(id_from, id_to)
+		self.__length += 1
+		return self.__dict[self.__length - 1]
+
+	def get_dict(self):
+		return self.__dict
+
+def parser_main_node(fname, conditions, directed = False):
 	if fname == None:
 		fname = '../dat/practice.txt'
 
-	udict = UserDict()
+	cond_list = []
+	for condition in conditions:
+		cond_list += [condition[0]] * int(condition[1])
+
+	random.shuffle(cond_list)
+	udict = Usr_dict()
 	func = lambda x, y: x.push_friend(y)
 
-	print('Loading ' + fname + ' file...')
+	print('Loading the graph file at ' + fname)
 	
 	with open(fname, 'r') as fin:
 		for line in fin:
-			nod_from, nod_to = map(udict.push_usr, map(int,line.strip().split(' ')))
+			nod_from, nod_to = [udict.push_usr(uid, cond_list)\
+								for uid in map(int, line.strip().split(' '))]
 			func(nod_from, nod_to)
 			if not directed: func(nod_to, nod_from)
 
-	print('Edges are successfully loaded!')
-	return udict
+	print('Loading completed\n')
+	return udict.dict
+
+def parser_main_edge(fname, conditions):
+	import random
+	if fname == None:
+		fname = '../dat/practice.txt'
+
+	cond_list = []
+	for condition in conditions:
+		cond_list += [condition[0]] * int(condition[1])
+
+	random.shuffle(cond_list)
+	edict = Edge_dict()
+	udict = Usr_dict()
+
+	print('Loading the graph file at ' + fname)
+	
+	with open(fname, 'r') as fin:
+		for line in fin:
+			id_from, id_to = map(int, line.strip().split(' '))
+			udict.push_usr(id_from, cond_list)
+			udict.push_usr(id_to, cond_list)
+			edict.push_edge(id_from, id_to)
+
+	print('Loading completed')
+	return edict.get_dict(), udict.dict
+
+if __name__ == '__main__':
+	udict = parser_main_node(None, [['yes', '4'], ['no', '5'], ['maybe', '0']])
+	for i in range(9):
+		print(udict[i+1].get_opinion(), end=' ')
--- src/odeGenerator.py
+++ src/odeGenerator.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     graph_path = None
     ode_path = None
     output_path = None
+    mode = None
     i = 1
     while i < len(argv):
         if argv[i] == '-graph':
@@ -19,43 +20,45 @@ if __name__ == '__main__':
         elif argv[i] == '-out':
             output_path = argv[i + 1]
             i += 2
+        elif argv[i] == '-mode':
+            mode = argv[i + 1]
+            i += 2
         else:
             print ('error parsing args')
 
+    print ('Loading the ode file...')
     workDataDict = odeParser.odeParser(ode_path)
     reactionComponents = [initParameters.split('=')[0].strip() for initParameters in workDataDict[2]['begin init']]
     opinionDict = {i:reactionComponents[i] for i in range(len(reactionComponents))}
 
-    graphDict = graphParser.parser_main(graph_path).dict
-
-
     for key,value in workDataDict.items():
         print(key,value)
-
-    print(reactionComponents) 
+    print ('Loading completed')
 
     #Here we can include a step in which we send this opinions to the graph opinion generator so we
     #can no which of the possible several opinions has been chosen by the user
 
+    conditions = []
+    for cond in workDataDict[2]['begin init']:
+        name, value = cond.split('=')
+        name = name.strip()
+        value = value.strip()
+        conditions.append([name, value])
+
+    # graphDict = graphParser.parser_main_node(graph_path, conditions)    
+    if mode == 'node':
+        graphDict = graphParser.parser_main_node(graph_path, conditions)
+    else:
+        edgeDict, graphDict = graphParser.parser_main_edge(graph_path, conditions)
+
     begin_initial = dict()
     initlist = []
 
-    for key,value in opinionDict.items():
-        print(key,value)
-
     for key,value in graphDict.items():
-        if value.get_opinion() == 1:
-            initlist.append('y' + str(key)+ ' = 1')
-            initlist.append('m' + str(key)+ ' = 0')
-            initlist.append('n' + str(key)+ ' = 0')
-        if value.get_opinion() == 0:
-            initlist.append('y' + str(key)+ ' = 0')
-            initlist.append('m' + str(key)+ ' = 1')
-            initlist.append('n' + str(key)+ ' = 0')
-        if value.get_opinion() == -1:
-            initlist.append('y' + str(key)+ ' = 0')
-            initlist.append('m' + str(key)+ ' = 0')
-            initlist.append('n' + str(key)+ ' = 1')  
+        for option, _ in conditions:
+            initlist.append(option + '_' +  str(key) + ' = ' +
+                            str(int(option == value.get_opinion())))
+
     begin_initial['begin init'] = initlist
 
     workDataDict[2] = begin_initial
@@ -63,36 +66,45 @@ if __name__ == '__main__':
     begin_reaction = dict()
 
     from itertools import product, permutations
-    def eq_writer(equation,idFrom,idTo):
+    def eq_writer(equation, idFrom, idTo):
         a = list(permutations([idFrom, idTo], 2))
         permutes = list(product(*[a,a]))
         res = []
         for i in range(4):
-            res.append(' '.join(equation.replace('yes', 'y{}').\
-                replace('no', 'n{}').replace('maybe', 'm{}').\
-                format(permutes[i][0][0], permutes[i][0][1],
-                       permutes[i][1][0], permutes[i][1][1]).\
-                split()))
+            eq = equation
+            for option, _ in conditions:
+                eq = eq.replace(option, option + '_{}')
+
+            res.append(' '.join(eq.format(permutes[i][0][0], permutes[i][0][1],
+                                          permutes[i][1][0], permutes[i][1][1]).\
+                                   split()))
         return res
 
-    from collections import deque
-        
-    curr_node = graphDict.popitem()[1]
-    eq_list = workDataDict[3]['begin reactions']
-    waiting_list = deque([])
     eq_list_new = []
-    while True:
-        curr_node.visiting()
-        for node in curr_node.get_friends():
-            if not node.is_visited():
-                for i in range(len(eq_list)):
-                    eq_list_new += eq_writer(eq_list[i], curr_node.get_uid(), node.get_uid())
-                waiting_list.append(node)
-        try:
-            curr_node = waiting_list.popleft()
-        except:
-            break
-
+    eq_list = workDataDict[3]['begin reactions']
+    print('Begin iterating...')
+    if mode == 'node':
+        curr_node = graphDict.popitem()[1]
+        while True:
+            curr_node.visiting()
+            for node in curr_node.get_friends():
+                if not node.is_visited():
+                    for i in range(len(eq_list)):
+                        eq_list_new += eq_writer(eq_list[i], curr_node.get_uid(), node.get_uid())
+            try:
+                curr_node = graphDict.popitem()[1]
+            except:
+                break
+    else:
+        while True:
+            try:
+                edge = edgeDict.popitem()[1]
+            except:
+                break
+            id_from, id_to = edge.get_nodes()
+            for i in range(len(eq_list)):
+                eq_list_new += eq_writer(eq_list[i], id_from, id_to)
+    print('Done generating ODEs')
     workDataDict[3]['begin reactions'] = eq_list_new
 
     odeParser.odeWriter(workDataDict, output_path)
